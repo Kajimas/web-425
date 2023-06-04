758d69c500d9a80c69b42b6c39cf1995ce2ff097
First Last <FL@academind.com>
2023-06-04 14:36:08 -0400

    fixed accidental changes

--- week-1/profile-app/src/app/app.component.ts
+++ week-1/profile-app/src/app/app.component.ts
@@ -3,9 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  isLoggedIn: Boolean = true;
-  assignment: string = 'Exercise 2.3 - Data Binding';
+  assignment: string = 'Assignment 1.5 - Components';
 }
