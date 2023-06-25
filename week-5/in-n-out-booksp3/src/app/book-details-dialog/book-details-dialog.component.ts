b08dd52042d87214d0c98d12ee4a80f4d1bc3a43
First Last <FL@academind.com>
2023-06-24 21:46:47 -0400

    Assignment 5.4 - Dialogs Update

--- week-5/in-n-out-booksp3/src/app/book-details-dialog/book-details-dialog.component.ts
+++ week-5/in-n-out-booksp3/src/app/book-details-dialog/book-details-dialog.component.ts
@@ -1,3 +1,10 @@
+/*
+Title: book-details-dialog.component.ts
+Author: William Egge
+Date: 22 June 2023
+Description: The book details dialog component ts file for in-n-out-books app.
+*/
+
 import { Component, OnInit, Inject } from '@angular/core';
 
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
