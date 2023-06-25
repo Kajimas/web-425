8000695b49b4ab737cfbe8240bc8e044afd79ce9
First Last <FL@academind.com>
2023-06-24 21:43:20 -0400

    Assignment 5.4 - Dialogs

---
+++ week-5/in-n-out-booksp3/src/app/book-details-dialog/book-details-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Inject } from '@angular/core';
+
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { IBook } from '../book.interface';
+
+@Component({
+  selector: 'app-book-details-dialog',
+  templateUrl: './book-details-dialog.component.html',
+  styleUrls: ['./book-details-dialog.component.scss'],
+})
+export class BookDetailsDialogComponent implements OnInit {
+  book: IBook;
+
+  constructor(
+    private dialogRef: MatDialogRef<BookDetailsDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) data
+  ) {
+    this.book = data.book;
+  }
+
+  ngOnInit(): void {}
+}
