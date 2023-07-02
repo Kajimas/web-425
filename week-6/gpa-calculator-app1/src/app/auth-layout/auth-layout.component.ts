b0b8e45e60cbc25d10792ba8086f7f46ade21db1
First Last <FL@academind.com>
2023-07-02 15:09:12 -0400

    Exercise 6.3 - Layouts

---
+++ week-6/gpa-calculator-app1/src/app/auth-layout/auth-layout.component.ts
@@ -0,0 +1,22 @@
+/*
+Title: auth-layout.component.ts
+Author: William Egge
+Date: 2 July 2023
+Description: The auth layout component ts file for gpa-calculator-app1 app.
+*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-layout',
+  templateUrl: './auth-layout.component.html',
+  styleUrls: ['./auth-layout.component.scss']
+})
+export class AuthLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ week-6/gpa-calculator-app1/src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+/*
+Title: home.component.ts
+Author: William Egge
+Date: 2 July 2023
+Description: The home component ts file for gpa-calculator-app1 app.
+*/
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
